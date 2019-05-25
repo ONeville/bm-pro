47c688c817fa19746ab5fa3d28cc5df3e683c92f
Orman <orman.neville@hotmail.com>
2019-05-25 00:42:52 -0500

    Deploy 05/25

--- docs/app/shared/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { PagerService } from './pager.service';
-import { SermonService } from './sermon.service';
-import { UtilsService } from './utils.service';
-
-export const SERVICES = [PagerService, SermonService, UtilsService];
-
-export * from './pager.service';
-export * from './sermon.service';
-export * from './utils.service';
